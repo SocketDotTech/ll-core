cddc78ff307cd8a56c13ede6241da288e4f12959
Aniket Sharma <aniket965.as@gmail.com>
2023-06-22 14:15:55 +0530

    feat: split middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { addresses } from './addresses'
-export { constants, Bridge, ChainId, Middleware  } from './constants'
+export { constants, Bridge, ChainId, Middleware, DexNames, RefuelDexNames, SimpleMiddleware  } from './constants'
 export { routes } from './routes'
 export * as abis from './abi'
